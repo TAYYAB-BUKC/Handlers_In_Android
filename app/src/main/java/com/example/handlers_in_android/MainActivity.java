c671425e3d1f79e8e6d436a8857f44062fc87ff8
TAYYAB DOST <tayyabdost@gmail.com>
2019-05-13 03:51:57 +0500

    SplashScreen Handler Code and creating intent for moving into NextActvitity after 6 Seconds.

--- app/src/main/java/com/example/handlers_in_android/MainActivity.java
+++ app/src/main/java/com/example/handlers_in_android/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.handlers_in_android;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASHSCREEN_DISPLAY_TIME = 6000; //It shift to NextActivity after 6 seconds
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, NextActivity.class);
+                startActivity(intent);
+            }
+        }, SPLASHSCREEN_DISPLAY_TIME);
     }
 }
